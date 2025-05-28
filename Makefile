2d6dbcda008b806df413a83ed14df5ed04274ebe
helouazizi <ouazizi2code@gmail.com>
2025-05-28 12:33:49 +0100

    add c file and linker

--- Makefile
+++ Makefile
@@ -1,12 +1,30 @@
-# Makefile for building and running a basic kernel
+# Makefile
 
-all: boot.bin
+NASM = nasm
+CC = gcc
+LD = ld
+OBJCOPY = objcopy
+QEMU = qemu-system-i386
 
-boot.bin: boot.asm
-	nasm -f bin boot.asm -o boot.bin
+all: os-image
 
-run: boot.bin
-	qemu-system-i386 -drive format=raw,file=boot.bin
+boot.o: boot.asm
+	$(NASM) -f elf32 boot.asm -o boot.o
+
+kernel.o: kernel.c
+	$(CC) -m32 -ffreestanding -c kernel.c -o kernel.o
+
+kernel.elf: boot.o kernel.o linker.ld
+	$(LD) -m elf_i386 -T linker.ld -o kernel.elf boot.o kernel.o
+
+kernel.bin: kernel.elf
+	$(OBJCOPY) -O binary kernel.elf kernel.bin
+
+os-image: kernel.bin
+	cp kernel.bin os-image.img
+
+run: os-image
+	$(QEMU) -drive format=raw,file=os-image.img
 
 clean:
-	rm -f boot.bin
+	rm -f *.bin *.o *.elf os-image.img
--- boot.asm
+++ boot.asm
@@ -1,74 +1,47 @@
-; bootloader.asm
-[org 0x7C00]
-[BITS 16]
+;[org 0x7C00]
+
+; GDT setup - 3 entries: null, code, data
+gdt_start:
+    dq 0
+    dq 0x00CF9A000000FFFF   ; code segment: base=0, limit=4GB, exec/read, ring0
+    dq 0x00CF92000000FFFF   ; data segment: base=0, limit=4GB, read/write, ring0
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
 
 start:
-    cli                 ; disable interrupts
+    cli
     xor ax, ax
     mov ds, ax
     mov es, ax
     mov ss, ax
     mov sp, 0x7C00
 
-    ; load GDT
-    lgdt [gdt_descriptor]
+    lgdt [gdt_descriptor]         ; load GDT
 
-    ; enter protected mode
     mov eax, cr0
     or eax, 1
-    mov cr0, eax
+    mov cr0, eax                 ; enable protected mode
 
-    ; far jump to clear pipeline and enter pm
-    jmp 0x08:protected_mode_start
+    jmp 0x08:protected_mode     ; far jump to flush pipeline & switch to protected mode
 
-; ---------------------------
-; GDT (Global Descriptor Table)
-gdt_start:
-    ; null descriptor
-    dq 0x0000000000000000
-
-    ; code segment (offset=0, base=0x00000000, limit=0xFFFFF, access=0x9A)
-    dq 0x00CF9A000000FFFF
-
-    ; data segment (offset=0, base=0x00000000, limit=0xFFFFF, access=0x92)
-    dq 0x00CF92000000FFFF
-
-gdt_end:
-
-gdt_descriptor:
-    dw gdt_end - gdt_start - 1   ; size
-    dd gdt_start                 ; address
-
-; ---------------------------
-; Protected Mode Code
-[BITS 32]
-protected_mode_start:
-    mov ax, 0x10       ; data segment selector
+[bits 32]
+protected_mode:
+    mov ax, 0x10
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
     mov ss, ax
-    mov esp, 0x90000
 
-    ; Print message to VGA
-    mov esi, message
-    mov edi, 0xB8000        ; VGA memory address
-.print:
-    lodsb
-    cmp al, 0
-    je .halt
-    mov ah, 0x1F            ; white on blue
-    stosw
-    jmp .print
+    extern kernel_main           ; tell assembler kernel_main is external (C)
 
-.halt:
-    cli
-    hlt
+    call kernel_main             ; call C kernel function
 
-message db "Hello from Ouazizi Kernel!", 0
+hang:
+    jmp hang                    ; infinite loop
 
-; ---------------------------
-; Boot Signature
 times 510 - ($ - $$) db 0
 dw 0xAA55
