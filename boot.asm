dbc32799a167805bdd2f1b1a8f6efd2f8349e08e
helouazizi <ouazizi2code@gmail.com>
2025-06-01 14:02:42 +0100

    print first hello into terminal

--- boot.asm
+++ boot.asm
@@ -1,4 +1,16 @@
 ; A simple boot sector program that loops forever
+    mov ah, 0x0e
+    mov al, 'H'
+    int 0x10 
+    mov al, 'e'
+    int 0x10 
+    mov al, 'l'
+    int 0x10 
+    mov al, 'l'
+    int 0x10 
+    mov al, 'o'
+    int 0x10 
+
 loop:
     jmp loop
 
